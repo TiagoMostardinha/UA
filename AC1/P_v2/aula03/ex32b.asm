7faa784c41ee0fc3bafc8fb5594e007ddfc6adf4
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2022-10-11 14:47:16 +0100

    ACI aula03

---
+++ AC1/P_v2/aula03/ex32b.asm
@@ -0,0 +1,58 @@
+# Mapa de registos:
+# $t0 – value
+# $t1 – bit
+# $t2 - i
+	.data
+str1:	.asciiz	"Introduza um numero: "
+str2:	.asciiz "\nO valor em binário e': "
+	.eqv	print_string,4
+	.eqv	read_int,5
+	.eqv	print_char,11
+	.text
+	.globl	main
+main: 	la	$a0,str1
+	li	$v0,print_string 	# (instrução virtual)
+	syscall 			# print_string(str1);
+	
+	li	$v0,read_int		# value=read_int();
+	syscall
+	move	$t0,$v0
+
+	la	$a0,str2		# print_string("...");
+	li	$v0,print_string
+	syscall
+	
+	li 	$t2,0			# i = 0
+	
+for:	bge	$t2,32,endfor		# while(i < 32) {
+	andi	$t1,$t0,0x80000000	# (instrução virtual)
+	
+	rem	$t4,$t2,4		# if((i % 4) == 0) // resto da divisão inteira
+	bne	$t4,$0,endif2
+	
+	li	$a0,' '			#print_char(' ');
+	li	$v0,print_char
+	syscall
+
+endif2:
+	beq	$t1,$0,else		# if(bit != 0)
+	
+	li	$a0,'1'			# print_char('1');
+	li	$v0,print_char
+	syscall
+	
+	j 	endif
+	
+else: 					# else
+	li	$a0,'0'			# print_char('0');
+	li	$v0,print_char
+	syscall
+
+endif:
+	sll	$t0,$t0,1		# value = value << 1;
+
+	addi	$t2,$t2,1		# i++;
+	
+	j	for			# }
+endfor: 
+	jr	$ra
