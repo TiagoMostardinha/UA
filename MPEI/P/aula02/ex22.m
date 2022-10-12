4341f8ac560e7804cc6ec524565c21ef3c2258e4
TiagoMostardinha <tiago.mostardinha@ua.pt>
2022-10-12 10:57:57 +0100

    MPEI aula03

--- MPEI/P/aula02/ex22.m
+++ MPEI/P/aula02/ex22.m
@@ -7,17 +7,43 @@ lancamentos = randi(100,20,N);
 
 for i = 1:N
     res(i) = length(unique(lancamentos(:,i))) == 20;
-end;
+end
 
 prob = sum(res)/N
 
 %% Codigo 2b
 clc;
 
+N = 1e5;
+
 lancamentos = randi(100,20,N);
 
 for i = 1:N
     res(i) = length(unique(lancamentos(:,i))) < 20;
-end;
+end
 
 prob = sum(res) / N
+
+%% Codigo 2c
+clc
+
+N = 1e5;
+
+lancamentos = randi(1000,20,N);
+
+for i = 1:N
+    res(i) = length(unique(lancamentos(:,i))) < 20;
+end
+
+prob = sum(res) / N
+
+%% Codigo 2d
+clc;
+
+N = 1e5;
+
+
+
+
+
+   
