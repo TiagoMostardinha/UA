90d5a06e5f925b78a807f85016b6bfa664cc1b2e
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2022-11-04 12:12:13 +0000

    ACI teste ex

--- AC1/P_v2/teste_exemplo/exte3.asm
+++ AC1/P_v2/teste_exemplo/exte3.asm
@@ -5,6 +5,8 @@
 # $t3: n_odd
 # $t4: p1Ultimo = p1+SIZE
 # $t5: p2Ultimo = p2+SIZE
+# $t6: *p1
+# $t7: *p2
 
 .data
 .eqv	N,5			# .eqv	N,35
@@ -12,6 +14,7 @@
 .eqv	read_int,5
 A:	.space 20		#A:	.space 140
 .align 2
+.space 4
 B:	.space 20		#B:	.space 140
 .align 2
 .text
@@ -29,10 +32,6 @@ li	$t4,N
 sll	$t4,$t4,2
 addu	$t4,$t4,$t0	# p1Ultimo = p1+SIZE
 
-li	$t5,N
-sll	$t5,$t5,2
-addu	$t5,$t5,$t1	# p2Ultimo = p2+SIZE 
-
 forr:
 bge	$t0,$t4,endforr	# for(p1 < a+N){
 
@@ -44,6 +43,45 @@ addiu	$t0,$t0,4	# p1++
 j	forr
 endforr:		# }
 
+la	$t0,A
+
+for:
+bge	$t0,$t4,endfor
+if:
+lw	$t6,0($t0)	# *p1
+rem	$t8,$t6,2	# *p1 % 2
+beq	$t8,0,else	# if *p1 % 2 != 0 then
+sw	$t6,0($t1)	# *p2 = *p1
+addiu	$t1,$t1,4	# *p2++
+addi	$t3,$t3,1	# n_odd++
+
+j	endif
+else:
+addi	$t4,$t4,1	# n_even++
+endif:
+addi	$t0,$t0,4	# p1++
+j	for
+endfor:
+
+la	$t1,B
+move	$t5,$t3
+sll	$t5,$t5,2
+addu	$t5,$t5,$t1	# p2Ultimo = p2+n_odd 
+
+forw:
+bge	$t1,$t5,endforw
+
+lw	$a0,0($t1)
+li	$v0,print_int10	
+syscall			# print_int10
+
+li	$a0,','
+li	$v0,11
+syscall
+
+addiu	$t1,$t1,4	# p2++
+j	forw
+endforw:
 
 
 jr	$ra
