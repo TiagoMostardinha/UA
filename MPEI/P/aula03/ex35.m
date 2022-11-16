b17f9f64602d8137093019eec7d8f9082dc622f8
TiagoMostardinha <tiago.mostardinha@ua.pt>
2022-11-16 10:57:55 +0000

    MPEI aula3

--- MPEI/P/aula03/ex35.m
+++ MPEI/P/aula03/ex35.m
@@ -6,7 +6,7 @@ T = [
         0.7 0.2 0.1;
         0.2 0.3 0.5;
         0.3 0.3 0.4
-    ]
+    ]';
 
 % Codigo 5b
 
@@ -21,14 +21,49 @@ end
 fprintf("p(2nd and 3rd day sun)=%f\n",T(1,1)^2)
 
 % Codigo 5c
+x1 = [1;0;0];
+x2 = T * x1;
 
-fprintf("p(2nd and 3rd day not rain)=%f\n",(1 - res(2,1)) * (1-res(2,2)));
+x2linha = [0.7;0.2;0];
+
+x2linha = x2linha / sum(x2linha);
+
+x3 = T * x2linha;
+fprintf("p(2nd and 3rd day not rain)=%f\n",sum((x2(1:2)) * sum(x3(1:2))));
 
 %Codigo 5d
-for i=1:1:31
+for i=1:30
+    res(:,i) = T^i * x0;
+end
+fprintf("D")
+res1 = [x0 res];
+fprintf("\naverage sun days: %f\n",sum(res1(1,:)))
+fprintf("average cloudy days: %f\n",sum(res1(2,:)))
+fprintf("average rainy days: %f\n",sum(res1(3,:)))
+
+%Codigo 5e
+
+x0 = [0;0;1];
+for i = 1:30
     res(:,i) = T^i * x0;
 end
 
-fprintf("\naverage sun days: %f\n",sum(res(1,:)))
-fprintf("average cloudy days: %f\n",sum(res(2,:)))
-fprintf("average rainy days: %f\n",sum(res(3,:)))
+res2 = [x0 res];
+fprintf("E")
+fprintf("\naverage sun days: %f\n",sum(res2(1,:)))
+fprintf("average cloudy days: %f\n",sum(res2(2,:)))
+fprintf("average rainy days: %f\n",sum(res2(3,:)))
+
+%Codigo 5f
+%res1 1o dia de sol
+fprintf("F1")
+fprintf("\naverage sun days: %f\n",sum(res1(1,:)) * 0.1)
+fprintf("average cloudy days: %f\n",sum(res1(2,:)) * 0.3)
+fprintf("average rainy days: %f\n",sum(res1(3,:)) * 0.5)
+
+%res2 1o dia de chuva
+fprintf("F2")
+fprintf("\naverage sun days: %f\n",sum(res(1,:)) * 0.1)
+fprintf("average cloudy days: %f\n",sum(res(2,:)) * 0.3)
+fprintf("average rainy days: %f\n",sum(res(3,:)) * 0.5)
+
