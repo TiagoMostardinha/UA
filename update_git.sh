e55040156cb206fb7190d0e6f220945be0d1fd7a
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2022-12-13 14:47:23 +0000

    Update

--- update_git.sh
+++ update_git.sh
@@ -2,7 +2,8 @@
 echo "checking for updates..."
 
 # Search for brancges
-if [ "$1" ==  "b" ]; then
+if [ ! -z "$1" ]; then
+	echo "setting for branches..."
 	git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
 fi
 
