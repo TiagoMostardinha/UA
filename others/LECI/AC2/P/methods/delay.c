e6e3b3f25040b178785dc82d7f576cfd0383ef02
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-03-17 17:03:24 +0000

    wtv

--- others/LECI/AC2/P/methods/delay.c
+++
@@ -1,9 +0,0 @@
-void delay(int ms) {
-    for (; ms > 0; ms--) {
-        resetCoreTimer();
-        
-        readCoreTimer();
-
-        while (readCoreTimer() < 20000);
-    }
-}
--- others/LECI/POO/P/src/aula08/ex1/Eletrico.java
+++
@@ -1,6 +0,0 @@
-package aula08.ex1;
-
-public interface Eletrico {
-    int autonomia();
-    void carregar(int percentagem);
-}
