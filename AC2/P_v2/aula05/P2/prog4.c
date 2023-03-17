01c22be0ec17cff015cf27bcb041d9e6e8c97784
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-03-17 16:42:27 +0000

    LECI_2Y

---
+++ AC2/P_v2/aula05/P2/prog4.c
@@ -0,0 +1,87 @@
+#include <detpic32.h>
+
+void delay(int ms)
+{
+    for (; ms > 0; ms--)
+    {
+        resetCoreTimer();
+        while (readCoreTimer() < 20000)
+            ;
+    }
+}
+
+unsigned char toBCD(unsigned char value){
+    return ((value / 10) << 4) + (value % 10);
+}
+
+void send2displays(unsigned char value)
+{
+    static const char display7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+
+    static char displayFlag = 0; // static variable: doesn't loose its value between calls to function
+
+    unsigned int dl = value & 0x0F;
+    unsigned int dh = value >> 4;
+
+    dl = display7Scodes[dl];
+    dh = display7Scodes[dh];
+
+    if (displayFlag == 0)
+    { // if "displayFlag" is 0 then send digit_low to display_low
+        LATD = (LATD & 0xff9f) | 0x0020;
+        LATB = (LATB & 0x80ff) | ((unsigned int)(dl)) << 8;
+    }
+    else
+    { // else send digit_high to didplay_high
+        LATD = (LATD & 0xff9f) | 0x0040;
+        LATB = (LATB & 0x80ff) | ((unsigned int)(dh)) << 8;
+    }
+    displayFlag = !displayFlag; // toggle "displayFlag" variable
+}
+
+int main(void)
+{
+    TRISB = (TRISB & 0x80ff) | 0x1; // declare variables
+    TRISD = TRISD & 0xff9f;
+    TRISE = TRISE & 0xFF00;
+    TRISC = TRISC & 0xBFFF;
+    // initialize ports
+    unsigned int counter = 0,i=0,K=0;
+    char bcd_counter;
+    while (1)
+    {
+        i = 0;
+        do
+        {
+            bcd_counter = toBCD(counter);
+            send2displays(bcd_counter);
+            delay(10); // wait 10 ms (1/100Hz)
+        } while (++i < K);
+        if ((PORTB & 0x0001) == 1){
+            counter++;      // increment counter (mod 60)
+            K = 50;
+        } else {
+            counter--;      // decrement counter (mod 60)
+            K = 20;
+        }
+
+        LATB = LATB & 0x80FF;
+
+        if (counter == 60){
+            counter = 0;
+            LATC = LATC | 0x4000;
+            delay(5000);
+            LATC = LATC & 0xBFFF;
+        }
+        if (counter == -1){
+            counter = 59;
+            LATC = LATC | 0x4000;
+            delay(5000);
+            LATC = LATC & 0xBFFF;
+        }
+
+        LATE = (LATE & 0xFF00) | bcd_counter;
+    }
+    return 0;
+}
+ 
---
+++ AC2/P_v2/methods/c/send2displays.c
@@ -0,0 +1,24 @@
+void send2displays(unsigned char value)
+{
+    static const char display7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+
+    static char displayFlag = 0; // static variable: doesn't loose its value between calls to function
+
+    unsigned int dl = value & 0x0F;
+    unsigned int dh = value >> 4;
+
+    dl = display7Scodes[dl];
+    dh = display7Scodes[dh];
+
+    if (displayFlag == 0)
+    { // if "displayFlag" is 0 then send digit_low to display_low
+        LATD = (LATD & 0xff9f) | 0x0020;
+        LATB = (LATB & 0x80ff) | ((unsigned int)(dl)) << 8;
+    }
+    else
+    { // else send digit_high to didplay_high
+        LATD = (LATD & 0xff9f) | 0x0040;
+        LATB = (LATB & 0x80ff) | ((unsigned int)(dh)) << 8;
+    }
+    displayFlag = !displayFlag; // toggle "displayFlag" variable
+}
